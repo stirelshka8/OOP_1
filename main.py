efd48f7faf90e94317b17356eac99cc999f09571
Evgenij Shka <stirelshka8@gmail.com>
2022-07-14 20:54:27 +0300

    Добавил комментарии и вернул часть кода

--- main.py
+++ main.py
@@ -14,15 +14,16 @@ class Mentor:
         self.courses_attached = []
      
  
-class Lecturer(Mentor):
+class Lecturer(Mentor): # Добавил подкласс Lecture
     def __init__(self, name, surname):
         super().__init__(name, surname)
+        self.grades = {} #  Атрибут-список для выставления оценок Lecture(в котором ключи – названия курсов, а значения – списки оценок)
 
-class Reviewer(Mentor):
+class Reviewer(Mentor): # Добавил подкласс Reviewer
     def __init__(self, name, surname):
         super().__init__(name, surname)
     
-    def rate_hw(self, student, course, grade):
+    def rate_hw(self, student, course, grade): # Данный метод перемещен из класса Mentor в подкласс Reviewer
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
                 student.grades[course] += [grade]
@@ -30,3 +31,16 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+# Зря ранее удалил код ниже! Оказался нужным)))
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student.courses_in_progress += ['Python']
+ 
+cool_mentor = Reviewer('Some', 'Buddy')
+cool_mentor.courses_attached += ['Python']
+ 
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_mentor.rate_hw(best_student, 'Python', 10)
+ 
+print(best_student.grades)
