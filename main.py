e106c5258fc57452ab6305927772e37bfd96aa98
Evgenij Shka <stirelshka8@gmail.com>
2022-07-14 14:26:12 +0300

    Deleting

--- main.py
+++ main.py
@@ -22,14 +22,3 @@ class Mentor:
         else:
             return 'Ошибка'
  
-best_student = Student('Ruoy', 'Eman', 'your_gender')
-best_student.courses_in_progress += ['Python']
- 
-cool_mentor = Mentor('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
- 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
- 
-print(best_student.grades)
