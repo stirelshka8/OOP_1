a2d013f28cae011910a8f96b213b96ae3603ae62
Evgenij Shka <stirelshka8@gmail.com>
2022-07-15 11:02:58 +0300

    .gitignore

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+from re import S
+from unicodedata import name
+
+
 class Student:
     def __init__(self, name, surname, gender):
         self.name = name
@@ -6,6 +10,19 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+
+    def print_grades(self):
+        print(self.grades)
+
+    def rate_lectur(self, lectur, course, grade):
+        if isinstance(lectur, Lecturer) and course in self.courses_in_progress and course in lectur.courses_attached:
+            if course in lectur.grades:
+                lectur.grades[course] += [grade]
+            else:
+                lectur.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
         
 class Mentor:
     def __init__(self, name, surname):
@@ -19,10 +36,15 @@ class Lecturer(Mentor): # Добавил подкласс Lecture
         super().__init__(name, surname)
         self.grades = {} #  Атрибут-список для выставления оценок Lecture(в котором ключи – названия курсов, а значения – списки оценок)
 
+        
+    def print_grades(self):
+        print(self.grades)
+
 class Reviewer(Mentor): # Добавил подкласс Reviewer
     def __init__(self, name, surname):
         super().__init__(name, surname)
-    
+
+   
     def rate_hw(self, student, course, grade): # Данный метод перемещен из класса Mentor в подкласс Reviewer
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -33,14 +55,30 @@ class Reviewer(Mentor): # Добавил подкласс Reviewer
             return 'Ошибка'
 
 # Зря ранее удалил код ниже! Оказался нужным)))
+cool_reviewer = Reviewer('Some', 'Buddy')
 best_student = Student('Ruoy', 'Eman', 'your_gender')
+cool_lecture = Lecturer('Bob', 'Bobson')
+
+cool_reviewer.courses_attached += ['Python']
+cool_lecture.courses_attached += ['Python']
+
 best_student.courses_in_progress += ['Python']
- 
-cool_mentor = Reviewer('Some', 'Buddy')
-cool_mentor.courses_attached += ['Python']
- 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
- 
-print(best_student.grades)
+
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+
+best_student.rate_lectur(cool_lecture, 'Python', 10)
+best_student.rate_lectur(cool_lecture, 'Python', 9)
+best_student.rate_lectur(cool_lecture, 'Python', 8)
+
+
+print(best_student.print_grades())
+print(cool_lecture.print_grades())
+print(cool_reviewer.__str__())
+
+
+
+
+
+
