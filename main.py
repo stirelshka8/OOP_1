70a16598601b9e5c856a12ce8dd0a06e50de84e5
Evgenij Shka <stirelshka8@gmail.com>
2022-07-15 11:07:01 +0300

    Добавил комментарии

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ class Student:
         else:
             return 'Ошибка'
 
-        
+    # TODO: Добавить перегрузку магического метода __str__
+       
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
@@ -32,14 +33,11 @@ class Lecturer(Mentor): # Добавил подкласс Lecture
         super().__init__(name, surname)
         self.grades = {} #  Атрибут-список для выставления оценок Lecture(в котором ключи – названия курсов, а значения – списки оценок)
 
-        
-    def print_grades(self):
-        print(self.grades)
+    # TODO: Добавить перегрузку магического метода __str__
 
 class Reviewer(Mentor): # Добавил подкласс Reviewer
     def __init__(self, name, surname):
         super().__init__(name, surname)
-
    
     def rate_hw(self, student, course, grade): # Данный метод перемещен из класса Mentor в подкласс Reviewer
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
@@ -50,7 +48,9 @@ class Reviewer(Mentor): # Добавил подкласс Reviewer
         else:
             return 'Ошибка'
 
-# Зря ранее удалил код ниже! Оказался нужным)))
+    # TODO: Добавить перегрузку магического метода __str__
+
+
 cool_reviewer = Reviewer('Some', 'Buddy')
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 cool_lecture = Lecturer('Bob', 'Bobson')
@@ -71,10 +71,3 @@ best_student.rate_lectur(cool_lecture, 'Python', 8)
 
 print(best_student.print_grades())
 print(cool_lecture.print_grades())
-print(cool_reviewer.__str__())
-
-
-
-
-
-
